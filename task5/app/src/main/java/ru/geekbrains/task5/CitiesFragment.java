54552912945ee861926d71cb8ab25a33e8ad97b6
Andrey Sozonov <Asozonov@yandex.ru>
2020-03-05 01:45:27 +0300

    Домашнее задание. Урок 8. Выход в интернет.

--- task5/app/src/main/java/ru/geekbrains/task5/CitiesFragment.java
+++ task5/app/src/main/java/ru/geekbrains/task5/CitiesFragment.java
@@ -7,24 +7,38 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
-import java.util.Arrays;
-import java.util.List;
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.stream.Collectors;
+import javax.net.ssl.HttpsURLConnection;
+import ru.geekbrains.task5.model.WeatherRequest;
 
 
 public class CitiesFragment extends Fragment {
 
+    private static final String TAG = "Weather";
+    private static final String WEATHER_API_KEY = "43dad43a0a9a1275403cb230692b0369";
     private boolean isTemperatureScreenExist;
     private static String currentCity;
     private CheckBox temperatureCheckBox;
     private CheckBox windCheckBox;
     private CheckBox pressureCheckBox;
     private TextInputEditText cityField;
+    private WeatherRequest currentWeatherRequest;
+    public static final String tempFieldKey = "TEMP_FIELD";
+    public static final String windFieldKey = "WIND_FIELD";
+    public static final String pressureFieldKey = "PRESSURE_FIELD";
     public static final String tempKey = "TEMP";
     public static final String windKey = "WIND";
     public static final String pressureKey = "PRESSURE";
@@ -41,51 +55,72 @@ public class CitiesFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         Button buttonMoscow = getView().findViewById(R.id.button_Moscow);
-        Button buttonStPetersburg = getView().findViewById(R.id.button_StPetersburg);
-        Button buttonNewYork = getView().findViewById(R.id.button_NewYork);
-        Button buttonLondon = getView().findViewById(R.id.button_London);
+        Button buttonRostov = getView().findViewById(R.id.button_Rostov);
+        Button buttonSochi = getView().findViewById(R.id.button_Sochi);
+        Button buttonVladivostok = getView().findViewById(R.id.button_Vladivostok);
         Button buttonParis = getView().findViewById(R.id.button_Paris);
-        Button buttonDubai = getView().findViewById(R.id.button_Dubai);
-        Button buttonEnter = getView().findViewById(R.id.enter);
+        Button buttonVoronezh = getView().findViewById(R.id.button_Voronezh);
+        final Button buttonEnter = getView().findViewById(R.id.enter);
         cityField = getView().findViewById(R.id.inputCityField);
 
         View.OnClickListener listener1 = new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                currentCity = ((Button) v).getText().toString();
-                showTemperatureScreen(currentCity);
+            public void onClick(final View v) {
+                if (v == buttonEnter) {
+                    currentCity = cityField.getText().toString();
+                } else {
+                    currentCity = ((Button) v).getText().toString();
+                }
+
+                try {
+                    final URL uri = new URL("https://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=metric&appid=" + WEATHER_API_KEY);
+                    final Handler handler = new Handler();
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            HttpsURLConnection urlConnection = null;
+                            try {
+                                urlConnection = (HttpsURLConnection) uri.openConnection();
+                                urlConnection.setRequestMethod("GET");
+                                urlConnection.setReadTimeout(10000);
+                                BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                                String result = getLines(in);
+                                Gson gson = new Gson();
+                                final WeatherRequest weatherRequest = gson.fromJson(result, WeatherRequest.class);
+                                currentWeatherRequest = weatherRequest;
+                                handler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        showTemperatureScreen(currentCity, weatherRequest);
+                                    }
+                                });
+                            } catch (Exception e) {
+                                if (v == buttonEnter) {
+                                    Snackbar.make(getView(), R.string.error, Snackbar.LENGTH_LONG).show();
+                                } else {
+                                    Snackbar.make(getView(), R.string.connection_error, Snackbar.LENGTH_LONG).show();
+                                }
+                                e.printStackTrace();
+                            } finally {
+                                if (null != urlConnection) {
+                                    urlConnection.disconnect();
+                                }
+                            }
+                        }
+                    }).start();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
             }
         };
 
         buttonMoscow.setOnClickListener(listener1);
-        buttonStPetersburg.setOnClickListener(listener1);
-        buttonNewYork.setOnClickListener(listener1);
-        buttonLondon.setOnClickListener(listener1);
+        buttonRostov.setOnClickListener(listener1);
+        buttonSochi.setOnClickListener(listener1);
+        buttonVladivostok.setOnClickListener(listener1);
         buttonParis.setOnClickListener(listener1);
-        buttonDubai.setOnClickListener(listener1);
-
-        final String[] cities = getResources().getStringArray(R.array.cities);
-        List<String> citiesList = Arrays.asList(cities);
-
-        buttonEnter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final String[] cities = getResources().getStringArray(R.array.cities);
-                String city = cityField.getText().toString();
-                boolean cityExist = false;
-                String errorMessage = getString(R.string.cityError);
-
-                for (int i=0; i< cities.length; i++) {
-                    if (city.equals(cities[i])){
-                        currentCity = city;
-                        showTemperatureScreen(currentCity);
-                        cityExist = true;
-                    }
-                }
-                if (cityExist) {cityField.setError(null);}
-                else {cityField.setError(errorMessage);}
-            }
-        });
+        buttonVoronezh.setOnClickListener(listener1);
+        buttonEnter.setOnClickListener(listener1);
     }
 
     @Override
@@ -98,7 +133,7 @@ public class CitiesFragment extends Fragment {
         }
         TemperatureFragment tempFrag = (TemperatureFragment) getFragmentManager().findFragmentById(R.id.temperature_screen);
         if (tempFrag != null) {
-            showTemperatureScreen(currentCity);
+            showTemperatureScreen(currentCity, currentWeatherRequest);
         }
 
     }
@@ -124,11 +159,15 @@ public class CitiesFragment extends Fragment {
 
     }
 
-    private void showTemperatureScreen(String city){
+    private void showTemperatureScreen(String city, WeatherRequest weatherRequest){
 
         temperatureCheckBox = getActivity().findViewById(R.id.temperature);
         windCheckBox = getActivity().findViewById(R.id.windSpeed);
         pressureCheckBox = getActivity().findViewById(R.id.atmospere);
+        float currentTemperature = weatherRequest.getMain().getTemp();
+        float currentWind = weatherRequest.getWind().getSpeed();
+        int currentPressure = weatherRequest.getMain().getPressure();
+
 
         if (isTemperatureScreenExist) {
             TemperatureFragment tempFrag = (TemperatureFragment) getFragmentManager().findFragmentById(R.id.temperature_screen);
@@ -138,7 +177,7 @@ public class CitiesFragment extends Fragment {
                 boolean tempCheckBoxIsChecked = temperatureCheckBox.isChecked();
                 boolean pressureCheckBoxIsChecked = pressureCheckBox.isChecked();
 
-                tempFrag = TemperatureFragment.create(city, tempCheckBoxIsChecked, windCheckBoxIsChecked, pressureCheckBoxIsChecked);
+                tempFrag = TemperatureFragment.create(city, tempCheckBoxIsChecked, windCheckBoxIsChecked, pressureCheckBoxIsChecked, currentTemperature, currentWind, currentPressure);
                 FragmentTransaction ft = getFragmentManager().beginTransaction();
                 ft.replace(R.id.temperature_screen, tempFrag);
                 ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
@@ -148,13 +187,21 @@ public class CitiesFragment extends Fragment {
         } else {
             Intent intent = new Intent();
             intent.setClass(getActivity(), TemperatureActivity.class);
-            intent.putExtra(tempKey, temperatureCheckBox.isChecked());
-            intent.putExtra(windKey, windCheckBox.isChecked());
-            intent.putExtra(pressureKey, pressureCheckBox.isChecked());
+            intent.putExtra(tempFieldKey, temperatureCheckBox.isChecked());
+            intent.putExtra(windFieldKey, windCheckBox.isChecked());
+            intent.putExtra(pressureFieldKey, pressureCheckBox.isChecked());
             intent.putExtra(cityKey, currentCity);
+            intent.putExtra(tempKey, currentTemperature);
+            intent.putExtra(windKey, currentWind);
+            intent.putExtra(pressureKey, currentPressure);
             startActivity(intent);
         }
     }
+
+    private String getLines(BufferedReader in) {
+        return in.lines().collect(Collectors.joining("\n"));
+    }
+
 }
 
 
